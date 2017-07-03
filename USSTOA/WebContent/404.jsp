39ce32e1b3eab3cd5a17a4fb200167f30fdc16b3
yangbolanybl <734612829@qq.com>
2017-07-03 09:10:23 +0800

    错误页面，监听器

---
+++ USSTOA/WebContent/404.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>404错误</title>
+</head>
+<body>
+<img alt="404" src="${pageContext.request.contextPath}/Image/404error.jpg">
+<a href="${pageContext.request.contextPath}/Logout">返回登录界面</a>
+</body>
+</html>
---
+++ USSTOA/src/com/listener/Listener.java
@@ -0,0 +1,39 @@
+package com.listener;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import dao.UserDao;
+import model.User;
+
+/**
+ * Application Lifecycle Listener implementation class MyListener
+ *
+ */
+@WebListener
+public class Listener implements HttpSessionListener {
+
+	/**
+	 * Default constructor.
+	 */
+	public Listener() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+	 */
+	public void sessionCreated(HttpSessionEvent arg0) {
+		System.out.println("sessionCreate!!!");
+		System.out.println();
+	}
+
+	/**
+	 * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+	 */
+	public void sessionDestroyed(HttpSessionEvent arg0) {
+		System.out.println("session过期，关闭数据库连接");
+	}
+
+}
