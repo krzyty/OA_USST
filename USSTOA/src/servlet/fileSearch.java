e64ebc4ea49a84738d04ef3922ba22f0ab8bcf12
lining95 <1075507782@qq.com>
2017-07-04 18:59:31 +0800

    文档管理servlet

---
+++ USSTOA/src/servlet/fileSearch.java
@@ -0,0 +1,41 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.File;
+
+import dao.FileDao;
+
+
+@WebServlet("/fileSearch")
+public class fileSearch extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public fileSearch() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		
+		String fileName=request.getParameter("fileName");
+		int ID=((Integer)request.getSession().getAttribute("ID")).intValue();
+		
+		List<File> searchFilesList= new FileDao().searchFiles(ID,fileName );
+		
+		request.setAttribute("searchFilesList", searchFilesList);
+		request.getRequestDispatcher("fileSearch.jsp").forward(request, response);
+	}
+
+}
